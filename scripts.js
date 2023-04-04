d88390f43eca671093c8ef63083b36b82018e5bc
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2023-04-04 14:47:28 -0300

    Creates new obstacles types

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+const obstacles = [
+  "dogshit",
+  "dogshit",
+  "dogshit",
+  "dogshit",
+  "dogshit",
+  "trash",
+  "trash",
+  "trash",
+  "hole",
+  "hole"
+];
+
 const man = document.getElementById("man");
-const shit = document.getElementById("shit");
+const obstacle = document.getElementById("obstacle");
 const landscape1 = document.getElementById("landscape1");
 const life = document.getElementById("life");
 const gameStatus = document.getElementById("status");
@@ -7,6 +20,8 @@ const score = document.getElementById("score");
 
 let gameState = "paused";
 let lifeCount = 5;
+let obstacleStartClassName = "dogshit-start";
+let obstacleCollision = false;
 
 function jump() {
   if (man.classList != "jump") {
@@ -32,16 +47,22 @@ let isAlive = setInterval(function () {
   let manTop = parseInt(window.getComputedStyle(man).getPropertyValue("top"));
 
   // get current cactus X position
-  let shitLeft = parseInt(
-    window.getComputedStyle(shit).getPropertyValue("left")
+  let obstacleLeft = parseInt(
+    window.getComputedStyle(obstacle).getPropertyValue("left")
   );
 
   // detect collision
-  if (shitLeft < 25 && shitLeft > 0 && manTop >= 150) {
+  if (
+    obstacleLeft < 25 &&
+    obstacleLeft > 0 &&
+    manTop >= 150 &&
+    !obstacleCollision
+  ) {
     // collision
+    obstacleCollision = true;
     if (lifeCount > 0) {
       // Timeout to avoid detecting multiple collisions in the same obstacle.
-      setTimeout(donothing, 50);
+      // setTimeout(donothing, 50);
       lifeCount = lifeCount - 1;
       updateLifeDiv();
     }
@@ -49,10 +70,14 @@ let isAlive = setInterval(function () {
     if (lifeCount <= 0) {
       gameStatus.classList.remove("hidden");
       landscape1.classList.remove("landscape-start");
-      shit.classList.remove("shit-start");
+      obstacle.classList.remove(obstacleStartClassName);
       man.classList.remove("walk");
       gameState = "gameover";
     }
+    // detect obstacle went out of screen
+  } else if (obstacleLeft < -10 && obstacleLeft >= -25) {
+    changeObstacle();
+    obstacleCollision = false;
   }
 
   if (gameState === "started") {
@@ -62,12 +87,20 @@ let isAlive = setInterval(function () {
   }
 }, 100);
 
+function changeObstacle() {
+  let idx = parseInt(Math.random() * 100) % obstacles.length;
+  obstacle.className = "";
+  obstacle.classList.add(obstacles[idx]);
+  obstacleStartClassName = obstacles[idx] + "-start";
+  obstacle.classList.add(obstacleStartClassName);
+}
+
 document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
     if (gameState === "paused" || gameState === "gameover") {
       gameStatus.classList.add("hidden");
       landscape1.classList.add("landscape-start");
-      shit.classList.add("shit-start");
+      obstacle.classList.add(obstacleStartClassName);
       man.classList.add("walk");
       score.textContent = "0";
       lifeCount = 5;
