417f9ace59bf805a20640a75d659daf331ee24d7
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2023-04-04 10:20:38 -0300

    Includes life indicator and its functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const man = document.getElementById("man");
 const shit = document.getElementById("shit");
 const landscape1 = document.getElementById("landscape1");
+const life = document.getElementById("life");
+const gameStatus = document.getElementById("status");
 
 let gameState = "paused";
-let life = 5;
+let lifeCount = 5;
 
 function jump() {
   if (man.classList != "jump") {
@@ -19,6 +21,11 @@ function jump() {
 
 function donothing() {}
 
+function updateLifeDiv() {
+  let lifeTotal = lifeCount * 25;
+  life.style.width = "" + lifeTotal + "px";
+}
+
 let isAlive = setInterval(function () {
   // get current dino Y position
   let manTop = parseInt(window.getComputedStyle(man).getPropertyValue("top"));
@@ -31,14 +38,14 @@ let isAlive = setInterval(function () {
   // detect collision
   if (shitLeft < 25 && shitLeft > 0 && manTop >= 150) {
     // collision
-    console.log("hit!");
-    if (life > 0) {
+    if (lifeCount > 0) {
       setTimeout(donothing, 50);
-      life = life - 1;
+      lifeCount = lifeCount - 1;
+      updateLifeDiv();
     }
 
-    console.log("isAlive.life=" + life);
-    if (life <= 0) {
+    if (lifeCount <= 0) {
+      gameStatus.classList.remove("hidden");
       landscape1.classList.remove("landscape-start");
       shit.classList.remove("shit-start");
       man.classList.remove("walk");
@@ -50,10 +57,12 @@ let isAlive = setInterval(function () {
 document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
     if (gameState === "paused" || gameState === "gameover") {
+      gameStatus.classList.add("hidden");
       landscape1.classList.add("landscape-start");
       shit.classList.add("shit-start");
       man.classList.add("walk");
-      life = 5;
+      lifeCount = 5;
+      updateLifeDiv();
       gameState = "started";
     } else {
       jump();
